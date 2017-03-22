824cfc36449d707c7af9c21b441dc7ba35564cbe
Orlando Rivero <orlandormx@gmail.com>
2017-03-22 17:16:38 -0600

    Ejercicio8.java cap3 resuelto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo3/Ejercicio8.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+Comprobar que los sufijos de octal y hexdecimal funcionan para los tipo
+long
+*/
+
+package com.orlandormx.capitulo3;
+import static net.mindview.util.Print.*;
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio8 {
+    
+    public static void main(String[] args) {
+        long l1 = 1L;
+        long l2 = 0x1L;
+        long l3 = 01;
+        
+        print(Long.toBinaryString(l1));
+        print(Long.toBinaryString(l2));
+        print(Long.toBinaryString(l3));
+    }
+    
+}

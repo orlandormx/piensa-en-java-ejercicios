7f46b83d9f0a71646e96e9058ce415f4a84f7438
Orlando Rivero <orlandormx@gmail.com>
2017-03-22 17:26:49 -0600

    Ejercicio9.java cap3 resuelto con dudas

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo3/Ejercicio9.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+valor mas grande y pequeño que se pueden representar con la notacion exponencial
+*/
+package com.orlandormx.capitulo3;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        float f1 = Float.MAX_VALUE;
+        
+        System.out.println(f1);
+    }
+}

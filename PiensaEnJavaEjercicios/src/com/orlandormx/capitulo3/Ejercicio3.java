67c59afaed4073382c9a8d7038ec60a59b29b27b
Orlando Rivero <orlandormx@gmail.com>
2017-03-22 15:43:51 -0600

    Ejercicio3.java cap3 resuelto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo3/Ejercicio3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+Creacion de alias en las llamadas a metodos
+*/
+package com.orlandormx.capitulo3;
+import static net.mindview.util.Print.*;
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio3 {
+    static void fMethod(Flotante f){
+        f.valor = 85.4f;
+    }
+    
+    public static void main(String[] args) {
+        Flotante f1 = new Flotante();
+        
+        f1.valor = 7.1f;
+        print("f1.valor="+f1.valor);
+        
+        fMethod(f1);
+        print("f1.valor="+f1.valor);
+    }
+    
+}

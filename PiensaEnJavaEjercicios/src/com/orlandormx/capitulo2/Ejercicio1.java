6313efac8d983ff80789652ee131a9972d971c30
Orlando Rivero <orlandormx@gmail.com>
2017-03-22 14:38:35 -0600

    Cambio de nombre de paquete

--- PiensaEnJavaEjercicios/src/com/orlandormx/capitulo2/Ejercicio1.java
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo2/Ejercicio1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.orlandormx.capitulo1;
+package com.orlandormx.capitulo2;
 
 /**
  *

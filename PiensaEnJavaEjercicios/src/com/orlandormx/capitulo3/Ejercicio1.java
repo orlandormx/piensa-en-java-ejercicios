039a55e251ea2c0378d28a92deb40568c02242d7
Orlando Rivero <orlandormx@gmail.com>
2017-03-22 15:04:50 -0600

    Ejercicio1.java resuelto

--- PiensaEnJavaEjercicios/src/com/orlandormx/capitulo3/Ejercicio1.java
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo3/Ejercicio1.java
@@ -3,12 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+Programa que usa la forma larga de impresion a consola y impresion corta,
+importando biblioteca estatica.
+*/
 package com.orlandormx.capitulo3;
 
+import static net.mindview.util.Print.*;
 /**
  *
  * @author developer
  */
 public class Ejercicio1 {
+    public static void main(String[] args){
     
+        print("Hola mundo");
+        
+        System.out.println("Hola mundo");
+    }
 }

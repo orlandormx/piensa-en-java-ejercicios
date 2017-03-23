8cfec552cb0db8e63da43bf9330bd51c294dec16
Orlando Rivero <orlandormx@gmail.com>
2017-03-23 11:40:41 -0600

    Ejercicio 1 y 2 cap4 resuelto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo4/Ej1y2y3y4y5.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orlandormx.capitulo4;
+import static com.orlandormx.util.Print.*;
+import java.util.Random;
+/**
+ *
+ * @author developer
+ */
+public class Ej1y2y3y4y5 {
+    public static void main(String[] args) {
+//        Ej1.imprimeValores();
+  //      Ej2.comparaValoresRand();
+    }
+}
+
+class Ej1{
+    static void imprimeValores(){
+        for(int i = 1; i <=100; i++){
+            print(i);
+        }
+    }
+}
+
+class Ej2{
+    static void comparaValoresRand(){
+        int[] valoresRand = new int[25];
+        Random r = new Random();
+        int cmp = r.nextInt(20);
+        for(int i = 0; i < valoresRand.length; i++){
+            valoresRand[i] = r.nextInt(20);
+        }
+        
+        for(int i = 0; i < valoresRand.length; i++){
+            if(valoresRand[i] > cmp)
+                print(valoresRand[i]+" > "+cmp);
+            else if(valoresRand[i] < cmp)
+                print(valoresRand[i]+" < "+cmp);
+            else
+                print(valoresRand[i]+" = "+cmp);
+        }
+    }
+}

74b88572f1383b3eba435d8e11f9d420a7542cdf
Orlando Rivero <orlandormx@gmail.com>
2017-03-24 12:24:59 -0600

    Ejercicio10y11y12 capitulo 5 inconclusos

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo5/Ejercicio10y11y12.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+Uso de metodo finalize(), no le entendi mucho quedan inconcluos estos ejercicios
+*/
+
+package com.orlandormx.capitulo5;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio10y11y12 {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class prueba{
+    protected void finalize(){
+    
+    //super.finalize();
+    }
+
+}

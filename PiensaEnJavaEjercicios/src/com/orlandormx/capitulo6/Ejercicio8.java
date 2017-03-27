8dc3d8d8cfc825fd91948f7c8921b9727b4f00bf
Orlando Rivero <orlandormx@gmail.com>
2017-03-27 10:35:32 -0600

    Ejercicio 8 capitulo 8 incompleto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo6/Ejercicio8.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*Ejercicio de patron singleton y similares*/
+package com.orlandormx.capitulo6;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Connection c = ConnectionManager.makeConnection();
+    }
+}
+
+class ConnectionManager{
+    ArrayList<Connection> col = new ArrayList<>(5);    
+    int count = 0;
+    
+    private ConnectionManager(){
+        for(int i = 0; i < col.size(); i++){
+            col.add(new Connection());
+        }
+    }
+
+    public static Connection makeConnection(){
+        return new Connection();
+    }
+}
+
+class Connection{
+   
+}

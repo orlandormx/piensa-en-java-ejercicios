90d906fbeb8cea503fdf8ab293b27af49a6a53c9
Orlando Rivero <orlandormx@gmail.com>
2017-03-28 16:58:19 -0600

    Ejercicio 1 capitulo 8

--- PiensaEnJavaEjercicios/src/com/orlandormx/capitulo8/Ejercicio1.java
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo8/Ejercicio1.java
@@ -4,11 +4,73 @@
  * and open the template in the editor.
  */
 package com.orlandormx.capitulo8;
-
+import static com.orlandormx.util.Print.*;
 /**
  *
  * @author developer
  */
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        Unicycle u = new Unicycle(1);
+        Bicycle b = new Bicycle(2);
+        Tricycle t = new Tricycle(3);
+        
+        PistaBicis.run(u);
+        PistaBicis.run(b);
+        PistaBicis.run(t);
+        
+    }
+}
+
+class Cycle{
+    private int llantas;
     
+    Cycle(int i){
+        llantas = i;
+    }
+    
+    void moveon(){
+        print("move cycle");
+    }
+}
+
+class Unicycle extends Cycle{
+    public Unicycle(int i){
+        super(i);
+    }
+    
+    @Override
+    void moveon(){
+        print("move unicycle");
+    }
+}
+
+class Bicycle extends Cycle{
+    public Bicycle(int i){
+        super(i);
+    }
+    @Override
+    void moveon(){
+        print("move bicycle");
+    }
+    
+    
+}
+
+class Tricycle extends Cycle{
+    public Tricycle(int i){
+        super(i);
+    }
+    
+    @Override
+    void moveon(){
+        print("move tricycle");
+    }
+}
+
+class PistaBicis{
+    static void run(Cycle c){
+        c.moveon();
+    }
+
 }

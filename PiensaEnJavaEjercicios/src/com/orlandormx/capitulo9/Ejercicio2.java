b82bbebb0f5fc740e875e927917c3fc8f9b3ccef
Orlando Rivero <orlandormx@gmail.com>
2017-04-03 10:20:01 -0500

    Ejercicio 2 capitulo 9 resuelto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo9/Ejercicio2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orlandormx.capitulo9;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio2 {
+    //NO se puede inicializar
+    //AbClass ab = new AbClass();
+}
+
+abstract class AbClass{
+
+}

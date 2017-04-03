e4c6697931b83c7f44b54d5c8d17947e51e74a3f
Orlando Rivero <orlandormx@gmail.com>
2017-04-03 13:25:16 -0500

    Ejercicio 5 y 6 capitulo 9 resuelto

---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo9/Ejercicio5y6.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orlandormx.capitulo9;
+
+import com.orlandormx.capitulo9.outer.Outer;
+
+/**
+ *
+ * @author developer
+ */
+public class Ejercicio5y6 implements Outer{
+
+    @Override
+    public void print() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void read() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void set() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo9/outer/Outer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.orlandormx.capitulo9.outer;
+/**
+ *
+ * @author developer
+ */
+public interface Outer{
+    void print();
+    void read();
+    void set();
+}

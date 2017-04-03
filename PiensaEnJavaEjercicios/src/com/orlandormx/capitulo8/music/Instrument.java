35f9ad48fcd493bd4e2ff035c6874ca97b0bd835
Orlando Rivero <orlandormx@gmail.com>
2017-04-03 17:06:13 -0500

    Ejercicio 9 capitulo 9 resuelto, se modifica archivo Instrument.java

--- PiensaEnJavaEjercicios/src/com/orlandormx/capitulo8/music/Instrument.java
+++ PiensaEnJavaEjercicios/src/com/orlandormx/capitulo8/music/Instrument.java
@@ -1,12 +1,9 @@
 //: polymorphism/music/Instrument.java
 package com.orlandormx.capitulo8.music;
-import static com.orlandormx.util.Print.*;
 
-class Instrument {
-  void play(Note n) {
-    print("Instrument.play() "+n);
-  }
-  public String toString() { return "Instrument"; }
-  void adjust() { print("Adjusting Instrument"); }
+abstract class Instrument {
+  abstract void play(Note n);
+  
+  abstract void adjust();
 }
  ///:~
